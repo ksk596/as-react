c64acb311d0e9cd58566c5845255ef61ffa6a55d
santhu.ksk596@gmail.com <santhu.ksk596@gmail.com>
2023-10-02 17:03:12 +0530

    part1 commit episode04

--- App.js
+++ App.js
@@ -1,65 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//Create Nested header element using React.createElement(h1,h2,h3 inside div with class title)
-// const header = React.createElement("div",{class: "title"},[
-// React.createElement("h1", {},"I'm H1 tag"),
-// React.createElement("h2", {},"I'm H2 tag"),
-// React.createElement("h3", {},"I'm H3 tag"),]);
-
-//Create same with JSX
-// const header = (
-//     <div className="title">
-//         <h1>I'm H1 tag</h1>
-//         <h2>I'm H2 tag</h2>
-//         <h3>I'm H3 tag</h3>
-//     </div>
-// )
 
 
-// Functional Component of the same JSX
-// const TitleComponent = () => {
-//     return (
-//         <div className="title">
-//             <h1>I'm H1 tag</h1>
-//             <h2>I'm H2 tag</h2>
-//             <h3>I'm H3 tag</h3>
-//         </div>
-//     );
-// };
+/**
+ * Header
+ *  - Logo
+ *  - Nav items
+ *  - Cart
+ * Body
+ *  - Search component
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ *          - Image
+ *          - Name of Res
+ *          - Star rating
+ *          - Cuisines
+ *          - Price
+ *          - Delivary time
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f5d51ef6af9b698ec1265b389f80b75c" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-//Composition of Components
-// const HeadingCompo = () => (
-//     <div id="container">
-//         <h1 className="heading">Fuctional Component </h1>
-//         <TitleComponent />
-//         {TitleComponent()}
-//         {<TitleComponent></TitleComponent>}
-//     </div>
-//);
+// const styleCard = {
+//     backgroundColor: "#f0f0f0"
+// }
+const RestaurantCard = (props) => {
+    console.log(props);
+    return (
+        <div className="res-card" style={{
+            backgroundColor: "#f0f0f0"
+        }}>
+            <img className="res-logo" alt-name="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gtflfeksgaqavpwayty2" />
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.3 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
 
-//Create Header component from scratch using fuctional component in Jsx
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          <RestaurantCard resName="Healthy Foods" cuisine="Biryani, Dosas, Chinese"/>
+          <RestaurantCard resName="KFC" cuisine="Burger, Chococake, coke"/>
+        </div>
+      </div>
+    );
+}
 
-const Header = () => (
-  <div id="container">
-    <img
-      className="logo"
-      src="https://img.icons8.com/?size=2x&id=bzf0DqjXFHIW&format=png"
-    />
-    <div className="search">
-        <input style={{width:150, height:40}} placeholder='Search' />
-    </div>
-    <img className='user' src='https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg' />
-  </div>
-);
-// const HeadingComponent = () => (
-//   <div id="container">
-//     <Title />
-//     <h1 className="heading">React functional component!</h1>
-//   </div>
-// );
+const AppLayout = () => { 
+    return (
+<div className="app">
+    <Header />
+    <Body />
+</div>
+)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<HeadingComponent />);
-//root.render(title);
-//root.render(<HeadingCompo />);
-root.render(<Header />);
+root.render(<AppLayout />);
